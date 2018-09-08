a19abaebf86ed69ed80ea97810c690672fe98dd3
Jeremiah Lübke <jer.luebke@web.de>
2018-09-08 20:04:43 +0200

    software runs but implementation is not doing what its supposed to...

--- game_of_life.c
+++ game_of_life.c
@@ -3,13 +3,13 @@
 #include <stdbool.h>
 #include <windows.h>
 #include "tigr.h"
+#include "parg.h"
 
 #define TITLE "Conway's Game of Life"
+#define HELP "Conway's Game of Life\nUsage: gol [-h] [-a STAY_ALIVE] [-b BE_BORN] [-d DELAY (1...1000)] [-f FILENAME] [-s SIZE (0, 2, 4, 8)]"
 
 const TPixel BLACK = {.r=0x00, .g=0x00, .b=0x00, .a=0xff};
 const TPixel WHITE = {.r=0xff, .g=0xff, .b=0xff, .a=0xff};
-int BE_BORN[] = {3, '\0'};
-int STAY_ALIVE[] = {2, 3, '\0'};
 
 
 typedef enum state {
@@ -18,53 +18,136 @@ typedef enum state {
 
 typedef struct Cell {
     state isAlive;
+    /* the TPixel attribute would not be requiered if not for some strange
+     * behaviour in `setNextState` (Details see comments there) */
     TPixel *pix;
     struct Cell *u, *ur, *r, *dr, *d, *dl, *l, *ul;
 } Cell;
 
+typedef struct Rule {
+    /* use pointer to arrays, because arrays will be allocated dynamically */
+    int **a, **b;
+    size_t a_len, b_len;
+} Rule;
 
-bool isWhite(TPixel *);
+
+static inline bool isWhite(TPixel *);
 
 void initCells(Cell *, Tigr *, int);
 
-void setNextState(Cell *) /*, int *, int *, int *, int *) */;
+bool setNextState(Cell *, Rule *);
 
-bool intInArray(int, int *, int *);
+bool intInArray(int, int *, size_t);
 
 
 int main(int argc, char **argv)
 {
-    Tigr *init = tigrLoadImage("initial.png");
+    /* stay alive, be born, filename (default values) */
+    char ac[8] = "23", bc[8] = "3", fn[100] = "initial.png";
+
+    /* parsing argv */
+    struct parg_state ps;
+    int c, i, delay = 500, size = 2;
+    parg_init(&ps);
+
+    while ((c = parg_getopt(&ps, argc, argv, "ha:b:d:f:s:")) != -1) {
+        switch (c) {
+        case 'h':
+            printf(HELP);
+            return EXIT_SUCCESS;
+        case 'a':
+            strncpy_s(ac, 8, ps.optarg, 8);
+            break;
+        case 'b':
+            strncpy_s(bc, 8, ps.optarg, 8);
+            break;
+        case 'd':
+            delay = atoi(ps.optarg);
+            if (!(1 <= delay && delay <= 1000)) {
+                fputs("delay must be in [1, 1000] (milisecs) ...", stderr);
+                return EXIT_FAILURE;
+            }
+            break;
+        case 'f':
+            strncpy_s(fn, 100, ps.optarg, 100);
+            break;
+        case 's':
+            size = atoi(ps.optarg);
+            if (!(size == 0 || size == 2 || size == 4 || size == 8)) {
+                /* pixel size: auto, double, three times, four times */
+                fputs("pixelsize must be in {0, 2, 4, 8} ...", stderr);
+                return EXIT_FAILURE;
+            }
+            break;
+        default: /* '?'*/
+            printf(HELP);
+            return EXIT_FAILURE;
+        }
+    }
+
+    size_t a_len = strlen(ac);
+    int *a = malloc(a_len * sizeof(*a));
+    for (i = 0; ac[i] != '\0'; ++i)
+        a[i] = ac[i] -'0';
+    size_t b_len = strlen(bc);
+    int *b = malloc(b_len * sizeof(*b));
+    for (i = 0; bc[i] != '\0'; ++i)
+        b[i] = bc[i] -'0';
+    Rule rule = { &a, &b, a_len, b_len };
+
+    printf("Using Rule %s/%s\nInitial conditions: %s\n", ac, bc, fn);
+
+    /* read inital conditions and start display */
+    Tigr *init = tigrLoadImage(fn);
     if (!init) {
-        fputs("Failed to open \"inital.png\". Terminating...", stderr);
-        return -1;
+        fprintf(stderr, "Failed to open \"%s\". Terminating...", fn);
+        return EXIT_FAILURE;
     }
 
     Tigr *screen = tigrWindow(init->w, init->h, TITLE, 0);
     tigrBlit(screen, init, 0, 0, 0, 0, init->w, init->h);
 
+    /* alloc and init cells */
     int tot = init->w * init->h;
-    Cell *cells = malloc(tot * sizeof(Cell));
+    Cell *cells = malloc(tot * sizeof(*cells));
     initCells(cells, screen, tot);
 
+    tigrUpdate(screen);
+    Sleep(1000);
+
+    /* mainloop */
+    int iter = 0;
+    bool changed;
     while (!tigrClosed(screen) && !tigrKeyDown(screen, TK_ESCAPE)) {
+        changed = false;
         for (Cell *cur = cells; cur != cells+tot; ++cur)
-            setNextState(cur) /* , &STAY_ALIVE[0], &STAY_ALIVE[2], &BE_BORN[0], &BE_BORN[1]) */;
+            changed = setNextState(cur, &rule) || changed;
 
         for (int i = 0; i < tot; ++i)
             screen->pix[i] = cells[i].isAlive ? BLACK : WHITE;
 
         tigrUpdate(screen);
-        Sleep(500);
+        Sleep(delay);
+        ++iter;
+        printf("Iteration: %d\r", iter);
+
+        if (!changed) {
+            printf("\n\nA static configuration was reached after %d iterations\n\n", iter);
+            system("PAUSE");
+            break;
+        }
     }
 
+    free(a);
+    free(b);
     free(cells);
     tigrFree(init);
-    return 0;
+
+    return EXIT_SUCCESS;
 }
 
 
-bool isWhite(TPixel *pix)
+static inline bool isWhite(TPixel *pix)
 {
     return (pix->r==0xff && pix->g==0xff && pix->b==0xff && pix->a==0xff) ? true : false;
 }
@@ -123,10 +206,19 @@ void initCells(Cell *cells, Tigr *screen, int tot)
     cells[w*(h-1)].dl = &cells[w-1];
 }
 
-void setNextState(Cell *cell) /*, int *alive_start, int *alive_end,
-        int *born_start, int *born_end) */
-{
+bool setNextState(Cell *cell, Rule *rule) {
+    bool changed = false;
     int alive = 0;
+    /* for some reason checking for cell->isAlive yields a completely different
+     * and unexpected result compared to !isWhite(cell->pix) ... */
+    /* alive += !isWhite(cell->u->pix) ? 1 : 0; */
+    /* alive += !isWhite(cell->ur->pix) ? 1 : 0; */
+    /* alive += !isWhite(cell->r->pix) ? 1 : 0; */
+    /* alive += !isWhite(cell->dr->pix) ? 1 : 0; */
+    /* alive += !isWhite(cell->d->pix) ? 1 : 0; */
+    /* alive += !isWhite(cell->dl->pix) ? 1 : 0; */
+    /* alive += !isWhite(cell->l->pix) ? 1 : 0; */
+    /* alive += !isWhite(cell->ul->pix) ? 1 : 0; */
     alive += cell->u->isAlive ? 1 : 0;
     alive += cell->ur->isAlive ? 1 : 0;
     alive += cell->r->isAlive ? 1 : 0;
@@ -136,16 +228,23 @@ void setNextState(Cell *cell) /*, int *alive_start, int *alive_end,
     alive += cell->l->isAlive ? 1 : 0;
     alive += cell->ul->isAlive ? 1 : 0;
 
-    if (cell->isAlive && !(alive == 2 || alive == 3))
+    if (cell->isAlive && !intInArray(alive, *(rule->a), rule->a_len)) {
         cell->isAlive = DEAD;
-    else if (!cell->isAlive && alive == 3)
+        changed = true;
+    } else if (!cell->isAlive && intInArray(alive, *(rule->b), rule->b_len)) {
         cell->isAlive = ALIVE;
+        changed = true;
+    }
+
+    return changed; 
 }
 
-inline bool intInArray(int i, int *start, int *end)
+inline bool intInArray(int s, int *arr, size_t len)
 {
     bool found = false;
-    for (; start != end; ++start)
-        found = (i == *start);
+    size_t i;
+
+    for (i = 0; i < len; ++i)
+        found = (s == arr[i]);
     return found;
 }
